eff0bd7861a1b2d40e926e60daee0efbcd2127f5
Oliver West <oliversimon.west@gmail.com>
2023-05-09 11:41:22 +0100

    fixed getAll response and added try catch block

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,8 +1,16 @@
 import { Prisma } from '@prisma/client';
 import { getAllUsers } from '../domain/user.js';
+import { sendDataResponse } from '../utils/responses.js';
 
 export const getAll = async (req, res) => {
-    const users = await getAllUsers();
-    console.log(users);
-    return users;
+    try {
+        const users = await getAllUsers();
+        if (users.length > 0) {
+            return sendDataResponse(res, 200, { users });
+        } else {
+            return sendDataResponse(res, 404, { error: 'User not found' });
+        }
+    } catch (e) {
+        return sendDataResponse(res, 500, { error: e.message });
+    }
 };
--- src/domain/user.js
+++ src/domain/user.js
@@ -2,6 +2,12 @@ import dbClient from '../utils/dbClient.js';
 // import bcrypt from 'bcrypt';
 
 export const getAllUsers = async () => {
-    const users = await dbClient.user.findMany();
+    const users = await dbClient.user.findMany({
+        select: {
+            id: true,
+            email: true,
+            role: true,
+        },
+    });
     return users;
 };
